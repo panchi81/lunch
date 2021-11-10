8a9f3443b36929bbac67c379f596c66486bf8502
FRAGO <frago@beijerelectronics.com>
2021-11-10 11:56:29 +0100

    stuf!

--- test.py
+++ test.py
@@ -21,23 +21,40 @@ def main():
 
     # print(spill)
 
-    mec_soup = BS(requests.get(urls[1]).content, "lxml")
-    # # mec_soup = BS(requests.get(urls[1]).content, "html.parser")
+    # mec_soup = BS(requests.get(urls[1]).content, "lxml")
+    mec_soup = BS(requests.get(urls[1]).text, "lxml")
     # mec_main = mec_soup.find_all("span", text=True)
 
-    # Cleanup those wonderfully broken html linebreaks.
+    # Cleanup wonderfully broken html (linebreaks and characters).
     for linebreak in mec_soup.find_all("br"):
         linebreak.extract()
-    mec = [
+
+    mec_menu = [
         t.text.strip()
         for t in mec_soup.find_all("span", text=True)[11:43]
         if t.text.strip()
     ]
     # # mec = [t.text for t in mec_main if "\xa0" not in t]
-    print(mec)
+    print(mec_menu)
 
     # # text = [x.get_text() for x in tag]
-    # # print(text)
+
+
+def get_parser(url: str) -> BS:
+    """
+    Request page and return a BeautifulSoup object
+
+    Args:
+        url (str): Lunch menu url
+
+    Returns:
+        BS: Lunch Menu
+    """
+    page_req = requests.get(url)
+    if page_req.status_code != 200:
+        raise IOError(f"Bad HTTP response code {page_req.status_code}")
+
+    return BS(page_req.text, "lxml")
 
 
 if __name__ == "__main__":
